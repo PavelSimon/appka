c2dedaf9f9e76d045cf26b67ed2f53cfd41affc1
Pavel Šimon <Pavel@pavel-simon.com>
2021-11-04 10:28:11 +0100

    úprava hlavného okna

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ inštalácia:
 2. py -m venv env - vytvorí virtuálne prostredie
 3. ./env/script/activate - spustenie VP
 4. pip install -r .\requirements.txt
+5. streamlit run .\app.py
--- app.py
+++ app.py
@@ -1,20 +1,8 @@
 import streamlit as st
-import moduls
 
-# Magic commands implicitly `st.write()`
-''' _This_ is some __Markdown__ '''
-a=3
-'dataframe:', a
-
-st.text('Fixed width text')
-st.markdown('_Markdown_') # see *
-st.caption('Balloons. Hundreds of them...')
-st.latex(r''' e^{i\pi} + 1 = 0 ''')
-st.write('Most objects') # df, err, func, keras!
-st.write(['st', 'is <', 3]) # see *
-st.title('My title')
-st.header('My header')
-st.subheader('My sub')
-st.code('for i in range(8): foo()')
-
-#* optional kwarg unsafe_allow_html = True
+st.title('Krypto evidencia')
+st.sidebar.subheader('hfkdshgf')
+col1,col2 = st.columns((3,1))
+col1.header('Hlavné okno')
+col1.subheader('nejaký text')
+col2.header("Nastavte parametre")
