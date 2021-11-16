344c67a7100e7ad796dda83b3cfebc12149febba
olibub2205 <bubniak.oliver@gmail.com>
2021-11-16 10:51:00 +0100

    UpdateTitle

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('Krypto evidencia')
+st.title('Krypto evidencia-doplnok')
 st.sidebar.subheader('hfkdshgf')
 col1,col2 = st.columns((3,1))
 col1.header('Hlavné okno')
