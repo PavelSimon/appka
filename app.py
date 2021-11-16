7cd0f582ff1fe34ea28f335e13dfb70a97bacc40
Pavel Šimon <Pavel@pavel-simon.com>
2021-11-16 10:45:45 +0100

    nastavenie apky

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Krypto manager", 
+    layout="wide", 
+    page_icon="", 
+    initial_sidebar_state="collapsed")
+    
+
 st.title('Krypto evidencia')
 st.sidebar.subheader('hfkdshgf')
 col1,col2 = st.columns((3,1))
