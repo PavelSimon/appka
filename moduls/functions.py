a224c0d5c059ec376f4a0600ab26e8c9f355e57b
Pavel Šimon <Pavel@pavel-simon.com>
2021-11-30 07:42:06 +0100

    vytvorený samostatný db modul

--- moduls/functions.py
+++ moduls/functions.py
@@ -1,13 +1,2 @@
-import mysql.connector
-from .config import server, user, password, db_name
-
-def connect_db():
-    conn = mysql.connector.connect(
-        host=server,
-        user=user,
-        password=password,
-        database=db_name
-    )     
-    return conn
 
 #ďalšie funcie
